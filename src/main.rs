3d5b50382a50a121a2709228b76dd7d7e8911c35
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-11-01 15:53:46 +0400

    tax with structs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,36 @@
-fn main() {
-    let mut years: [i32; 3] = [2020, 2021, 2022];
+struct City {
+    description: String,
+    residents: u64,
+    is_coastal: bool,
+}
 
-    let first_year = years[0];
-    let second_year = years[1];
-    let third_year = years[2];
-
-    println!("first_year: {}, second_year: {}, third_year: {}", first_year, second_year, third_year);
-
-    years[0] = 2023;
-    println!("first_year: {}", years[0]);
-
-    for year in years.iter() {
-        println!("year: {}", year);
+fn new_city(residents: u64, is_coastal: bool) -> City {
+    if is_coastal {
+        City {
+            description: format!("a *coastal* city of approximately {} residents", residents),
+            residents,
+            is_coastal,
+        }
+    } else {
+        City { description: (format!("{} *non-coastal* with residents is not a coastal city", residents)), residents, is_coastal }
     }
 }
 
+fn main() {
+    let rustville: City = new_city(123, false);
+    let springfield: City = new_city(75000, false);
 
+    println!("This city can be described as: {}", rustville.description);
+    if rustville.is_coastal {
+        println!("It is a coastal city.");
+    } else {
+        println!("It is not a coastal city.");
+    }
+
+    println!("\nThis city can be described as: {}", springfield.description);
+    if springfield.is_coastal {
+        println!("It is a coastal city.");
+    } else {
+        println!("It is not a coastal city.");
+    }
+}
