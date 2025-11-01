0b16ec1b24b0b7e3590a5653500d274add921ab8
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-11-01 10:40:43 +0400

    add calculation using helper functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
 fn main() {
-    let mut x = 1.1;
-    println!("x is {}, first print", x);
-    x = 2.2;
-    println!("x is {}, second print", x);
+    let add_result = add_both(1.1, 2.2);
+    let subtract_result = subtract_both(1.1, 2.2);
+    let multiply_result = multiply_both(1.1, 2.2);
+    let divide_result = divide_both(1.1, 2.2);
+
+    println!("add_result is {}", add_result);
+    println!("subtract_result is {}", subtract_result);
+    println!("multiply_result is {}", multiply_result);
+    println!("divide_result is {}", divide_result);
+
+    let general_result = format!("add: {}, subtract: {}, multiply: {}, divide: {}", add_result, subtract_result, multiply_result, divide_result);
+
+    println!("general_result is {}", general_result);
 }
 
+fn multiply_both(x: f64, y: f64) -> f64 {
+    return  x * y;
+}
+
+fn add_both(x: f64, y: f64) -> f64 {
+    return x + y;
+}
+
+fn subtract_both(x: f64, y: f64) -> f64 {
+    return x - y;
+}
+
+fn divide_both(x: f64, y: f64) -> f64 {
+    return x / y;
+}
