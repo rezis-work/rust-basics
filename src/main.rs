d80909407339b85cc41839e3386460b623d34f07
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-11-01 10:24:33 +0400

    implement basing stings with interpolation, variables, format and print

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let firstname = "Rezi";
+    let lastname = "Karanadze";
+    let greeting = format!("Hello, {} {}! nice to meet you!", firstname, lastname);
+    println!("{}", greeting);
+}
