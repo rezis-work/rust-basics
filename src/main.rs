c7f7960d12114b73155e38e13f09c6867adb25bd
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-11-02 13:42:54 +0400

    implement enums and pattern matching

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,27 @@
-struct City {
-    description: String,
-    residents: u64,
-    is_coastal: bool,
-}
-
-fn new_city(residents: u64, is_coastal: bool) -> City {
-    if is_coastal {
-        City {
-            description: format!("a *coastal* city of approximately {} residents", residents),
-            residents,
-            is_coastal,
-        }
-    } else {
-        City { description: (format!("{} *non-coastal* with residents is not a coastal city", residents)), residents, is_coastal }
-    }
-}
-
 fn main() {
-    let rustville: City = new_city(123, false);
-    let springfield: City = new_city(75000, false);
+    let current_color = Color::Yellow;
 
-    println!("This city can be described as: {}", rustville.description);
-    if rustville.is_coastal {
-        println!("It is a coastal city.");
-    } else {
-        println!("It is not a coastal city.");
+    match current_color {
+        Color::Green => {
+            println!("It was green!")
+        }
+        Color::Yellow => {
+            println!("It was yellow!")
+        }
+        Color::Red => {
+            println!("It was red!")
+        }
+        Color::Custom(r, g, b) => {
+            println!("It was custom! {} {} {}", r, g, b)
+        }
     }
 
-    println!("\nThis city can be described as: {}", springfield.description);
-    if springfield.is_coastal {
-        println!("It is a coastal city.");
-    } else {
-        println!("It is not a coastal city.");
-    }
+
+}
+
+enum Color {
+    Green,
+    Yellow,
+    Red,
+    Custom (u8, u8, u8)
 }
