d760111f4c94115e82aa1a7215a36a27e9d4fc98
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-11-02 13:57:25 +0400

    implement type parameters

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,10 @@
 fn main() {
-    let red = Color::new(255, 0, 0);
-    let purple = Color::new(255, 0, 255);
-    let (r, g, b) = Color::rgb(purple);
-    let (r2, g2, b2) = red.rgb();
-    println!("Red: {}, Green: {}, Blue: {}", r, g, b);
-    println!("Red: {}, Green: {}, Blue: {}", r2, g2, b2);
+    let success: Result<i32, String> = Result::Ok(42);
+    let error: Result<i32, String> = Result::Err("Something went wrong".to_string());
 }
 
-enum Color {
-    Green,
-    Yellow,
-    Red,
-    Custom (u8, u8, u8)
+enum Result<T, E> {
+    Ok(T),
+    Err(E),
 }
 
-impl Color {
-    fn rgb(self) -> (u8, u8, u8) {
-        return match self {
-            Color::Green => (0, 255, 0),
-            Color::Yellow => (255, 255, 0),
-            Color::Red => (255, 0, 0),
-            Color::Custom(r, g, b) => (r, g, b),
-        };
-    }
-    fn new(r: u8, g: u8, b: u8) -> Self {
-        Color::Custom(r, g, b)
-    }
-}
