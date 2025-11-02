6437beb51e385035aa6e86fd09f206d9331c3254
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-11-02 23:00:04 +0400

    implement task about enums and methods

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,60 @@
+enum CitySize {
+    Town,
+    City,
+    Metropolis,
+}
+
+struct City {
+    description: String,
+    residents: u64,
+    is_coastal: bool,
+}
+
+impl City {
+    fn new(city_size: CitySize, is_coastal: bool) -> Self {
+        let (description, residents) = match city_size {
+            CitySize::Town => {
+                let residents = 1_000;
+
+                (
+                    format!("a *town* of approximately {} residents", residents),
+                    residents
+                )
+            }
+            CitySize::City => {
+                let residents = 10_000;
+
+                (
+                    format!("a *city* of approximately {} residents", residents),
+                    residents
+                )
+            }
+            CitySize::Metropolis => {
+                let residents = 1_000_000;
+
+                (
+                    format!("a *metropolis* of approximately {} residents", residents),
+                    residents
+                )
+            }
+        };
+
+        City {
+            description,
+            residents,
+            is_coastal,
+        }
+    }
+}
+
 fn main() {
-    let success: Result<i32, String> = Result::Ok(42);
-    let error: Result<i32, String> = Result::Err("Something went wrong".to_string());
-}
+    let tbilisi = City::new(CitySize::Metropolis, true);
 
-enum Result<T, E> {
-    Ok(T),
-    Err(E),
-}
+    println!("This city is called {} and has {} residents", tbilisi.description, tbilisi.residents);
 
+    if tbilisi.residents > 100_000 {
+        println!("This city is a city!");
+    } else {
+        println!("This city is a town!");
+    }
+}
